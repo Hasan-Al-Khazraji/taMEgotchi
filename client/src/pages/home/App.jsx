af632752e17d2e8ba8653db860fed975fd6b1926
Hasan Al-Khazraji <halkhazr@uoguelph.ca>
2025-01-11 22:27:43 -0500

    add mepet check and update UI for authenticated users

--- client/src/pages/home/App.jsx
+++ client/src/pages/home/App.jsx
@@ -1,13 +1,57 @@
 import LoginButton from "../../components/loginButton";
-import Profile from "../../components/profile";
 import LogoutButton from "../../components/logoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  async function mepetExists() {
+    try {
+      const response = await fetch("http://localhost:5000/api/mepet", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if (data.mepet) {
+        window.location.href = "/mepet";
+        return data.mepet;
+      }
+      else {
+        window.location.href = "/register";
+        return false;
+      }
+    } catch (error) {
+      console.error("Error checking if mepet exists:", error);
+      return false;
+    }
+  }
+
   return (
-    <div className="App">
-      <LoginButton></LoginButton>
-      <Profile></Profile>
-      <LogoutButton></LogoutButton>
+    <div className="bg-slate-900 h-screen text-white flex flex-col justify-center items-center font-bold">
+      {isAuthenticated ?
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-2xl">Welcome back {user.name} 👋</h2>
+          <img src={user.picture} alt="PFP" className="rounded-full my-3" />
+          <h3 className="text-xl">Ready to see your Mepet?</h3>
+          <button
+            className="rounded-lg bg-emerald-500 px-3 py-2 my-2 hover:bg-emerald-600 hover:scale-110 transform transition duration-100"
+            onClick={() => mepetExists()}
+          >
+            Take me!
+          </button>
+          <span className="absolute bottom-0 right-0 m-4 bg-red-400 rounded-lg p-2 hover:bg-red-500 hover:scale-110 transform transition duration-100">
+            <LogoutButton></LogoutButton>
+          </span>
+        </div>
+        :
+        isLoading ? <h1>Loading...</h1> :
+          <span className="bg-emerald-500 rounded-lg p-2 hover:bg-emerald-600 hover:scale-110 transform transition duration-100">
+            <LoginButton></LoginButton>
+          </span>
+      }
     </div>
   );
 }
--- server/server.py
+++ server/server.py
@@ -123,6 +123,10 @@ def logout():
         )
     )
 
+@app.route("/api/mepet")
+def mepet():
+    return jsonify({"mepet": ""})
+
 @app.route("/")
 def home():
     return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
